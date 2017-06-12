cf673f3490f711bd6bb2813b796b9b1c65ec97a5
Rafael Galdamez <rafaelgaldamez@Rafaels-MacBook-Pro.local>
2017-06-11 20:16:39 -0700

    final code

---
+++ assets/javascript/app.js
@@ -0,0 +1,106 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAA9VpP5La5ysc0JTlsMuZQVuWxvyGsoS4",
+  authDomain: "train-schedule-68ef7.firebaseapp.com",
+  databaseURL: "https://train-schedule-68ef7.firebaseio.com",
+  projectId: "train-schedule-68ef7",
+  storageBucket: "train-schedule-68ef7.appspot.com",
+  messagingSenderId: "634275696503"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//BUtton for adding Train
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  //Grabs user input
+  var trainName = $("#train-input").val().trim();
+  var destName = $("#destination-input").val().trim();
+  var firstTrainTime = $("#firsttrain-input").val().trim();
+  var freqTime = $("#frequency-input").val().trim();
+
+  //create local "temporary" object for holding train data
+  var newTrain = {
+    train: trainName,
+    destination: destName,
+    firstTrain: firstTrainTime,
+    frequency: freqTime
+  };
+
+  //Uploads train data to the database
+  database.ref().push(newTrain);
+
+  //Log all to the console
+  console.log(newTrain.train);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTrain);
+  console.log(newTrain.frequency);
+
+  //alert
+  alert("Train Added!");
+
+  //Clears all of the text-boxes
+  $("#train-input").val("");
+  $("#destination-input").val("");
+  $("#firsttrain-input").val("");
+  $("#frequency-input").val("");
+});
+
+//Create Firebase event for adding train to the database and a row in the html when a user adds Train
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  //store everything into a variable
+  var trainName = childSnapshot.val().train;
+  var destName = childSnapshot.val().destination;
+  var firstTrainTime = childSnapshot.val().firstTrain;
+  var freqTime = childSnapshot.val().frequency;
+
+  //Train information
+  console.log(trainName);
+  console.log(destName);
+  console.log(firstTrainTime);
+  console.log(freqTime);
+
+
+  //Prettify the train start
+  // var firstTrainTimePretty = moment.unix(firstTrainTime).format("HH:mm");
+
+  // //calculate the time to the next train using math to calculate the difference between the added train and now
+  // var firstTrainTimeConverted = moment(firstTrainTime, "HH:mm");
+  // console.log(firstTimeConverted);
+
+  //Current time
+  var currentTime = moment();
+  console.log("Current Time: " + moment(currentTime).format("HH:mm"));
+
+  //Difference between the times
+  var diffTime = moment().diff(moment(firstTrainTime), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  //Time apart (remainder)
+  var tRemainder = diffTime % freqTime;
+  console.log("tRemainder : " + tRemainder);
+
+  //Minute until train
+  var tMinutesTillTrain = freqTime - tRemainder;
+  console.log("Minutes until train: " + tMinutesTillTrain);
+
+  //Next TRAIN
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
+
+  var tillTrainPrettify = moment(tMinutesTillTrain).format("mm");
+
+  $("#train-table > tbody").append("<tr>" +
+  "<td>" + trainName + "</td><td>" + destName + "</td>" +
+  "<td>" + freqTime + "</td>" +
+  "<td>" + nextTrain + "</td>" +
+  "<td>" + tillTrainPrettify + "/td>" +
+  "</tr>"
+  );
+
+})
